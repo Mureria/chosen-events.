b19feafde1e6e67fa546bf1009eae2dd062d1b73
Mureria <chosenireri42@gmail.com>
2023-08-01 10:32:04 +0300

    service pg

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -2,12 +2,32 @@
 const Services = () => {
   return (
     <>
-    <div className="w-full h-screen relative mx-auto justify-center items-center text-white py-6 bg-fixed bg-center bg-cover sev">
+    <div className="w-full h-full relative mx-auto justify-center items-center text-white py-6 bg-fixed bg-center bg-cover sev md:h-screen">
         <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]' />
         <div className=" z-[2] text-white ">
                 <h1 className="text-4xl text-center font-bold">Our Services</h1>
                 <div className="w-[100px] my-2 mx-auto border-b-2 "></div>
-            <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
+            <div className="max-w-[1240px] justify-center items-center my-4 px-4 mx-auto grid gap-5 md:grid-cols-4">
+              <div className="border p-4 mt-10 ">
+                <h1 className="font-semibold text-3xl mb-3 font-sans">Ruracio</h1>
+                <p className="font-thin text-base tracking-widest">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis aliquam et doloribus ratione itaque!</p>
+                <p className="border px-8 py-2 mt-8 ">Show more</p>
+              </div>
+              <div className="border p-4 mt-10 ">
+                <h1 className="font-semibold text-3xl mb-3 font-sans">Ruracio</h1>
+                <p className="font-thin text-base tracking-widest">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis aliquam et doloribus ratione itaque!</p>
+                <p className="border px-8 py-2 mt-8 ">Show more</p>
+              </div>
+              <div className="border p-4 mt-10 ">
+                <h1 className="font-semibold text-3xl mb-3 font-sans">Ruracio</h1>
+                <p className="font-thin text-base tracking-widest">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis aliquam et doloribus ratione itaque!</p>
+                <p className="border px-8 py-2 mt-8 ">Show more</p>
+              </div>
+              <div className="border p-4 mt-10 ">
+                <h1 className="font-semibold text-3xl mb-3 font-sans">Ruracio</h1>
+                <p className="font-thin text-base tracking-widest">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis aliquam et doloribus ratione itaque!</p>
+                <p className="border px-8 py-2 mt-8 ">Show more</p>
+              </div>
 
             </div>
                 
